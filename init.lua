629ab9aee4f55e841953e20a0f5ad1aab5a555f2
aquapi <aquaplmc@gmail.com>
2023-08-17 13:38:51 +0700

    Initial

---
+++ init.lua
@@ -0,0 +1,33 @@
+if vim.loader and vim.fn.has "nvim-0.9.1" == 1 then vim.loader.enable() end
+
+for _, source in ipairs {
+  "astronvim.bootstrap",
+  "astronvim.options",
+  "astronvim.lazy",
+  "astronvim.autocmds",
+  "astronvim.mappings",
+} do
+  local status_ok, fault = pcall(require, source)
+  if not status_ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault) end
+end
+
+if astronvim.default_colorscheme then
+  if not pcall(vim.cmd.colorscheme, astronvim.default_colorscheme) then
+    require("astronvim.utils").notify(
+      ("Error setting up colorscheme: `%s`"):format(astronvim.default_colorscheme),
+      vim.log.levels.ERROR
+    )
+  end
+end
+
+require("astronvim.utils").conditional_func(astronvim.user_opts("polish", nil, false), true)
+
+-- Load color scheme
+vim.cmd [[colorscheme nord]]
+
+-- Set tab
+vim.opt.tabstop = 4        -- The width of a TAB is set to 4.
+vim.opt.shiftwidth = 4     -- Indents will have a width of 4
+vim.opt.softtabstop = 4    -- Sets the number of columns for a TAB
+vim.opt.expandtab = true   -- Expand TABs to spaces
+
---
+++ lua/plugins/community.lua
@@ -0,0 +1,14 @@
+return {
+    "AstroNvim/astrocommunity",
+    { import = "astrocommunity.pack.typescript" },
+    { import = "astrocommunity.pack.cpp" },
+    { import = "astrocommunity.pack.svelte" },
+    { import = "astrocommunity.pack.docker" },
+    { import = "astrocommunity.pack.html-css" },
+    { import = "astrocommunity.pack.toml" },
+    { import = "astrocommunity.pack.json" },
+    { import = "astrocommunity.pack.markdown" },
+    { import = "astrocommunity.pack.bash" },
+    
+    { import = "astrocommunity/colorscheme/nord-nvim", enabled = true },
+} 
