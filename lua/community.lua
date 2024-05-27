fe357cdf534a9ecd08fe671676b4efb461da7f63
aquapi <aquaplmc@gmail.com>
2024-05-27 07:58:16 +0700

    New config

---
+++ lua/community.lua
@@ -0,0 +1,18 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.colorscheme.catppuccin" },
+
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.zig" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.astro" }
+}
